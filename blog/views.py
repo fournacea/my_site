94cff5c6d55331604f6277a37519a31293bb352b
Adam Fournace <fournacea@gmail.com>
2023-07-03 10:09:26 -0400

    Display comments

--- blog/views.py
+++ blog/views.py
@@ -59,6 +59,7 @@ class PostDetailView(View):
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id"),
         }
 
         return render(request, "blog/post-detail.html", context)
@@ -81,6 +82,7 @@ class PostDetailView(View):
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id"),
         }
 
         return render(request, "blog/post-detail.html", context)
